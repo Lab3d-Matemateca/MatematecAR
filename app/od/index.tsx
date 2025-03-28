00b35fddcb063f7d5f93ba8217d662d625528dd9
Xnths <jonathascastilhoes@gmail.com>
2025-03-28 12:57:09 -0300

    feat: adding object detection

--- app/od/index.tsx
+++ app/od/index.tsx
@@ -2,12 +2,26 @@ import { Button } from '@/components/button';
 import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Camera, useCameraDevice, useCameraPermission } from "react-native-vision-camera"
+import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from "react-native-vision-camera"
+
+import { Label, Object, useImageLabeler, useObjectDetector } from "react-native-vision-camera-mlkit"
 
 
 export default function App() {
   const device = useCameraDevice("back")
   const {hasPermission} = useCameraPermission()
+  const { imageLabeler } = useImageLabeler()
+  const { objectDetector } = useObjectDetector()
+
+  const [labels, setLabels] = useState<Label[]>([]);
+  const [objects, setObjects] = useState<Object[]>([]);
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    "worklet";
+    const labels = imageLabeler(frame)
+    const objects = objectDetector(frame);
+    console.log(objects)
+  }, []);
 
   if (!hasPermission) {
     return <View />
@@ -29,8 +43,7 @@ export default function App() {
       style={StyleSheet.absoluteFill}
       device={device}
       isActive={true}
-    >
-      <Button>Identificar</Button>
-    </Camera>
+      frameProcessor={frameProcessor}
+    />
   );
 }
