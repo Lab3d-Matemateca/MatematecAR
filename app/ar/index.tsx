fb8117f31e1adb2bea2b4e14f5f9d2afb76ab61b
Xnths <jonathascastilhoes@gmail.com>
2025-04-07 18:48:04 -0300

    feat: adding image tracking

--- app/ar/index.tsx
+++ app/ar/index.tsx
@@ -1,56 +1,74 @@
 import {
-    ViroARScene,
-    ViroARSceneNavigator,
-    ViroText,
-    ViroTrackingReason,
-    ViroTrackingStateConstants,
-  } from "@reactvision/react-viro";
-  import React, { useState } from "react";
-  import { StyleSheet } from "react-native";
-  
-  const HelloWorldSceneAR = () => {
-    const [text, setText] = useState("Initializing AR...");
-  
-    function onInitialized(state: any, reason: ViroTrackingReason) {
-      console.log("onInitialized", state, reason);
-      if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-        setText("Hello World!");
-      } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-        // Handle loss of tracking
-      }
+  ViroARImageMarker,
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroARTrackingTargets,
+  ViroText,
+  ViroTrackingReason,
+  ViroTrackingStateConstants,
+} from "@reactvision/react-viro";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState("Initializing AR...");
+  const [position, setPosition] = useState();
+
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    console.log("onInitialized", state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText("Hello World!");
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      // Handle loss of tracking
     }
-  
-    return (
-      <ViroARScene onTrackingUpdated={onInitialized}>
+  }
+
+  ViroARTrackingTargets.createTargets({
+    uspCard: {
+      source: require("@/assets/usp.png"),
+      orientation: "Up",
+      physicalWidth: 0.85,
+    },
+  });
+
+  return (
+    <ViroARScene>
+      <ViroARImageMarker
+        onAnchorUpdated={(anchor) => {
+          setPosition(anchor.position);
+        }}
+        target="uspCard"
+      >
         <ViroText
           text={text}
           scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, -1]}
+          position={[0, 0, 0]}
           style={styles.helloWorldTextStyle}
         />
-      </ViroARScene>
-    );
-  };
-  
-  export default () => {
-    return (
-      <ViroARSceneNavigator
-        autofocus={true}
-        initialScene={{
-          scene: HelloWorldSceneAR,
-        }}
-        style={styles.f1}
-      />
-    );
-  };
-  
-  var styles = StyleSheet.create({
-    f1: { flex: 1 },
-    helloWorldTextStyle: {
-      fontFamily: "Arial",
-      fontSize: 30,
-      color: "#ffffff",
-      textAlignVertical: "center",
-      textAlign: "center",
-    },
-  });
+      </ViroARImageMarker>
+    </ViroARScene>
+  );
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-import { ExternalLink } from "@/app-example/components/ExternalLink";
 import { Button, NavButton } from "@/components/button";
 import { Card } from "@/components/card";
 import { Section, SectionTitle } from "@/components/section";
@@ -6,52 +5,64 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import { Image, Pressable, Text, View } from "react-native";
 
-const imeLogo = require("@/assets/images/ime-logo.png")
+const imeLogo = require("@/assets/images/ime-logo.png");
 
 export default function Index() {
   return (
-    <View
-      className="flex flex-col w-screen bg-[#f5f4f8] h-screen p-4 gap-8"
-    >
+    <View className="flex flex-col w-screen bg-[#f5f4f8] h-screen p-4 gap-8">
       <View className="flex flex-row justify-between w-full items-center">
         <FontAwesome size={48} name="circle" color="black" />
         <View className="flex flex-col justify-center items-center">
-          <Text className="text-3xl text-[#404040]">matema<Text className="font-extrabold">tecAR</Text></Text>
+          <Text className="text-3xl text-[#404040]">
+            matema<Text className="font-extrabold">tecAR</Text>
+          </Text>
           <Text className="text-xs">EXPLORE, APRENDA, DIVIRTA-SE!</Text>
         </View>
-        <Image style={{width: 50, height: 50}} source={imeLogo} />
+        <Image style={{ width: 50, height: 50 }} source={imeLogo} />
       </View>
       <Card className="bg-[#0079c2] py-[48px]">
         <View className="flex-1 flex flex-col items-center justify-center gap-4">
-              <View className="flex flex-col items-center">
-                  <Text className="text-[100px] font-extrabold text-white leading-none">AR</Text>
-                  <Text className="text-[10px] font-extrabold text-white leading-none">REALIDADE AUMENTADA</Text>
-              </View>
-              <NavButton href="/ar" className="bg-[#6ca8cd]">Começar</NavButton>
+          <View className="flex flex-col items-center">
+            <Text className="text-[100px] font-extrabold text-white leading-none">
+              AR
+            </Text>
+            <Text className="text-[10px] font-extrabold text-white leading-none">
+              REALIDADE AUMENTADA
+            </Text>
           </View>
-          <View className="flex-1 flex items-center justify-center">
-              <FontAwesome size={100} name="cube" color="white" />
-          </View>
+          <NavButton href="/ar" className="bg-[#6ca8cd]">
+            Começar
+          </NavButton>
+        </View>
+        <View className="flex-1 flex items-center justify-center">
+          <FontAwesome size={100} name="cube" color="white" />
+        </View>
       </Card>
       <Section>
         <SectionTitle>Precisa de ajuda?</SectionTitle>
-        <NavButton href="/tutorial" className="w-full bg-[#7ac142]">Como usar o aplicativo?</NavButton>
+        <NavButton href="/tutorial" className="w-full bg-[#7ac142]">
+          Como usar o aplicativo?
+        </NavButton>
       </Section>
       <Section>
         <SectionTitle>Conheça os nossos sites!</SectionTitle>
         <View className="flex flex-row gap-4">
-          <ExternalLink href="https://matemateca.ime.usp.br/">
+          <Link href="https://matemateca.ime.usp.br/">
             <Card className="bg-[#eaaa08] w-fit flex flex-col gap-4">
               <FontAwesome size={100} name="cube" color="white" />
-              <Text className="text-[16px] font-extrabold text-white leading-none">Matemateca</Text>
+              <Text className="text-[16px] font-extrabold text-white leading-none">
+                Matemateca
+              </Text>
             </Card>
-          </ExternalLink>
-          <ExternalLink href="https://docs.google.com/document/d/1OuxJp8augSKuZ6rLnI4MrBjUvQvX9GsL_PDsuVp_O3o/edit?tab=t.0">
+          </Link>
+          <Link href="https://docs.google.com/document/d/1OuxJp8augSKuZ6rLnI4MrBjUvQvX9GsL_PDsuVp_O3o/edit?tab=t.0">
             <Card className="bg-[#f47920] w-fit flex flex-col gap-4">
               <FontAwesome size={100} name="cube" color="white" />
-              <Text className="text-[16px] font-extrabold text-white leading-none">PUB 2024</Text>
+              <Text className="text-[16px] font-extrabold text-white leading-none">
+                PUB 2024
+              </Text>
             </Card>
-          </ExternalLink>
+          </Link>
         </View>
       </Section>
     </View>
